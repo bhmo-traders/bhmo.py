82619bb0843b3835850d6675182190e755fd1138
IAMIHAH <kr.ihah@kakao.com>
2022-12-03 21:27:50 +0900

    make project file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='bhmo',
+    packages=find_packages(),
+    version='0.0.1-dev',
+    description='API library of Discord farm game bot, BHMo',
+    author='bhmo-traders',
+    license='MIT',
+)
